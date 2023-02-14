bd143d5c6c491de74beef1259ee8eca6575a8e45
Alvaro-SP <3034161730108@ingenieria.usac.edu.gt>
2023-02-13 21:09:57 -0600

    Corrección de operación en endpoint de números
    Se realizó una corrección rápida en el endpoint que sumaba dos números, ya que en realidad debía multiplicarlos. Se actualizó el código correspondiente para que la operación se realice correctamente y se verificó su funcionamiento.

--- api.py
+++ api.py
@@ -2,11 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/suma', methods=['POST'])
+@app.route('/multiplicar', methods=['POST'])
 def sumar():
     num1 = request.json['num1']
     num2 = request.json['num2']
-    resultado = num1 + num2
+    resultado = num1 * num2
     return jsonify({'resultado': resultado})
 
 @app.route('/carne', methods=['GET'])
